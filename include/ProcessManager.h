860eeaf5d593aba96656953418a3613a5b1ebef0
aceone <henols@gmail.com>
2025-03-11 13:59:14 +0100

    Still panicking

--- include/ProcessManager.h
+++ include/ProcessManager.h
@@ -1,55 +1,31 @@
 #ifndef PROCESSMANAGER_H
 #define PROCESSMANAGER_H
 
-#include <cstddef>
-// #include <mutex>  // Include mutex header
+#include <esp_log.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/semphr.h>
+
+#include <cstddef>
+
 #include "IProcessHandler.h"
 #include "Stream.h"
-#include <esp_log.h>
-
 
 class ProcessManager {
-   public:
-    // Construct with an array of IProcessHandler pointers and their count.
-    ProcessManager(IProcessHandler **handlers, size_t count);
-
-    // Process the stream: first byte decides if it's a handshake.
-    // If the first byte is '@', read the command string and call handshake().
-    // Otherwise, dispatch the data via process().
-    // Returns true if a handler handled data.
-    virtual bool process();
-    virtual bool process(Stream *stream);
-
-    // Use the mutex to protect changes to stream.
-    bool registerStream(Stream *stream) {
-        xSemaphoreTake(streamMutex, portMAX_DELAY);
-        ESP_LOGI(TAG, "Registering stream.");
-        this->stream = stream;
-        xSemaphoreGive(streamMutex);
-        return true;
-    }
-
-    bool unregisterStream(Stream *stream) {
-        xSemaphoreTake(streamMutex, portMAX_DELAY);
-        bool res = this->stream == stream;
-        ESP_LOGI(TAG, "Unregistering stream: %s", res ? "true" : "false");
-        if (res) {
-            this->stream = nullptr;
-            // return true;
-        }
-        xSemaphoreGive(streamMutex);
-        return res;
-    }
-
    private:
     IProcessHandler **_handlers;
     size_t _count;
     Stream *stream;
-    // mutable std::mutex streamMutex; // Mutex to guard stream access
     SemaphoreHandle_t streamMutex = NULL;
-    static const char *TAG ;
+    bool process(Stream *stream);
+    static constexpr const char *TAG = "ProcessManager";
+
+   public:
+    ProcessManager(IProcessHandler **handlers, size_t count);
+
+    bool process();
+
+    bool registerStream(Stream *stream);
+    bool unregisterStream(Stream *stream);
 };
 
 #endif  // PROCESSMANAGER_H
--- src/ProcessManager.cpp
+++ src/ProcessManager.cpp
@@ -1,59 +1,83 @@
 #include "ProcessManager.h"
 
+
+#include <esp_log.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/semphr.h>
 #include <freertos/task.h>
 
 #include <cstddef>
 
-const char* ProcessManager::TAG = "ProcessManager";
+#include "IProcessHandler.h"
+#include "Stream.h"
 
-ProcessManager::ProcessManager(IProcessHandler** handlers, size_t count)
-    : _handlers(handlers), _count(count), stream(nullptr) {
-    streamMutex = xSemaphoreCreateMutex();
-}
-
-bool ProcessManager::process() {
-    bool res = false;
-    if (stream != nullptr && xSemaphoreTake(streamMutex, 0) == pdTRUE) {
-        if (stream != nullptr) {
-            res = process(stream);
-        }
-        ESP_LOGI(TAG, "ProcessManager giving back mutex.");
-        xSemaphoreGive(streamMutex);
-        ESP_LOGI(TAG, "ProcessManager mutex given back.");
+ProcessManager::ProcessManager(IProcessHandler **handlers, size_t count)
+        : _handlers(handlers), _count(count), stream(nullptr) {
+        streamMutex = xSemaphoreCreateMutex();
     }
-    return res;
-}
 
-bool ProcessManager::process(Stream* localStream) {
-    ESP_LOGI(TAG, "ProcessManager processing stream.");
-    int avl = localStream->available();
-    if (avl < 1) {
-        // localStream->write("no data available\n", 18);
-        // ESP_LOGI(TAG, "ProcessManager no data available %d.",avl);
+    bool ProcessManager::process() {
+        bool res = false;
+        if (stream != nullptr && xSemaphoreTake(streamMutex, 0) == pdTRUE) {
+            if (stream != nullptr) {
+                res = process(stream);
+            }
+            ESP_LOGI(TAG, "ProcessManager giving back mutex.");
+            xSemaphoreGive(streamMutex);
+            ESP_LOGI(TAG, "ProcessManager mutex given back.");
+        }
+        return res;
+    }
+
+
+    bool ProcessManager::process(Stream *localStream) {
+        ESP_LOGI(TAG, "ProcessManager processing stream.");
+        int avl = localStream->available();
+        if (avl < 1) {
+            localStream->write("no data available\n", 18);
+            // ESP_LOGI(TAG, "ProcessManager no data available %d.",avl);
+            return false;
+        }
+        ESP_LOGI(TAG, "ProcessManager data available %d.", avl);
+        int firstByte = localStream->read();
+
+        if (firstByte == '@') {
+            ESP_LOGI(TAG, "ProcessManager received handshake marker.");
+            // std::string cmd = stream->readStringUntil(' ');
+
+            // for (size_t i = 0; i < _count; i++) {
+            //     if (_handlers[i]->handshake(cmd, localStream)) {
+            //         return true;
+            //     }
+            // }
+        } else if (firstByte == '#') {
+            ESP_LOGI(TAG, "ProcessManager received process marker.");
+            // for (size_t i = 0; i < _count; i++) {
+            //     ProcessStatus status = _handlers[i]->process();
+            //     if (status != ProcessStatus::NotProcess) {
+            //         return true;
+            //     }
+            // }
+        }
         return false;
     }
-    ESP_LOGI(TAG, "ProcessManager data available %d.", avl);
-    int firstByte = localStream->read();
 
-    if (firstByte == '@') {
-        ESP_LOGI(TAG, "ProcessManager received handshake marker.");
-        // std::string cmd = stream->readStringUntil(' ');
-
-        // for (size_t i = 0; i < _count; i++) {
-        //     if (_handlers[i]->handshake(cmd, localStream)) {
-        //         return true;
-        //     }
-        // }
-    } else if (firstByte == '#') {
-        ESP_LOGI(TAG, "ProcessManager received process marker.");
-        // for (size_t i = 0; i < _count; i++) {
-        //     ProcessStatus status = _handlers[i]->process();
-        //     if (status != ProcessStatus::NotProcess) {
-        //         return true;
-        //     }
-        // }
+    bool ProcessManager::registerStream(Stream *stream) {
+        xSemaphoreTake(streamMutex, portMAX_DELAY);
+        ESP_LOGI(TAG, "Registering stream.");
+        this->stream = stream;
+        xSemaphoreGive(streamMutex);
+        return true;
     }
-    return false;
-}
+
+    bool ProcessManager::unregisterStream(Stream *stream) {
+        xSemaphoreTake(streamMutex, portMAX_DELAY);
+        bool res = this->stream == stream;
+        ESP_LOGI(TAG, "Unregistering stream: %s", res ? "true" : "false");
+        if (res) {
+            this->stream = nullptr;
+            // return true;
+        }
+        xSemaphoreGive(streamMutex);
+        return res;
+    }
